effaaefa88028c20e7c4132bebf52060201e875f
bdfoster <bdfoster@endigotech.com>
2012-09-06 01:08:06 -0400

    public layout code cleanup

--- views/footer.php
+++
@@ -1,8 +0,0 @@
-
-<hr>
-
-<footer class="footer">
-  <div class="container-fluid">
-    <p><center>&copy; 2012 Janet Foster. All rights reserved.</center></p>
-  </div>
-</footer>
--- views/layout.php
+++ views/layout.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <title>Art by Janet Foster · Bunkhouse-Art.com</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="root" >
+    <?php Bootstrap::set_charset('utf-8'); ?>
+    <?php Bootstrap::set_title('Art by Janet Foster · Bunkhouse-Art.com'); ?>
+    <?php Bootstrap::make_meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
+    <?php Bootstrap::make_meta('description'); ?>
+    <?php Bootstrap::make_meta('author'); ?>
 
 
     <!-- Stylesheets/CSS -->
-    <link href="<?php echo $this->make_route('/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo $this->make_route('/css/master.css'); ?>" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo $this->make_route('/css/bootstrap-responsive.min.css'); ?>" rel="stylesheet" type="text/css"/>
+    <?php Bootstrap::add_css($this->make_route('/css/bootstrap.min.css')); ?>
+    <?php Bootstrap::add_css($this->make_route('/css/master.css')); ?>
+    <?php Bootstrap::add_css($this->make_route('/css/bootstrap-responsive.min.css')); ?>
 
     <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <?php Bootstrap::add_script('http://html5shim.googlecode.com/svn/trunk/html5.js') ?>
     <![endif]-->
 
   </head>
@@ -22,7 +22,33 @@
     
     <!-- Header -->
     <div class="container-fluid">
-      <?php include(ROOT . '/views/header.php'); ?>
+      <div class="navbar navbar-fixed-top">
+        <div class="navbar-inner">
+          <div class="container-fluid">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <div class="nav-collapse collapse">
+              <ul class="nav pull-left">
+                <?php Bootstrap::make_navbar_button('$this->make_route('/')', 'Home'); ?>
+              </ul>
+              <ul class="nav pull-right">
+                <?php 
+                if (User::is_authenticated()) {
+                  Bootstrap::make_navbar_button($this->make_route('/logout'), 'Logout');
+                } else {
+                  Bootstrap::make_navbar_button($this->make_route('/login'), 'Login'); 
+                } ?>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="page-header">
+        <h1><small>Art by </small>Janet Foster</h1>
+      </div>
     </div>
     
     <!-- Content -->
@@ -30,27 +56,33 @@
       <!-- Alerts -->
       <?php echo $this->display_alert('error'); ?>
       <?php echo $this->display_alert('success'); ?>
-        <?php include($this->content); ?>
+      <?php include($this->content); ?>
     </div>
     
     <!-- Footer -->
     <div class="container-fluid">
-      <?php include(ROOT . '/views/footer.php'); ?>
+      <hr>
+      <div class="footer">
+        <div class="container-fluid pagination-centered">
+          <p>&copy; 2012 Janet Foster. All rights reserved.</p>
+        </div>
+      </div>
     </div>
     
     <!-- Scripts -->
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/jquery.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-transition.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-alert.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-modal.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-carousel.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-button.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-affix.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-typeahead.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-tooltip.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-tab.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/bootstrap-collapse.js'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $this->make_route('/js/master.js'); ?>"></script>
+    <?php Bootstrap::add_script($this->make_route('/js/jquery.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-transition.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-alert.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-modal.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-carousel.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-button.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-affix.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-typeahead.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-tooltip.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-tab.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/bootstrap-collapse.js'), 'text/javascript'); ?>
+    <?php Bootstrap::add_script($this->make_route('/js/master.js'), 'text/javascript'); ?>
+    
   </body>
 
 </html>
