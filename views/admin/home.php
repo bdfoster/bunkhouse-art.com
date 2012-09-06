4cd352f9dc861982b93303f82280a87e73a93c72
bdfoster <bdfoster@endigotech.com>
2012-09-06 13:16:33 -0400

    fix center issue on admin home

--- views/admin/home.php
+++ views/admin/home.php
@@ -1,7 +1,7 @@
 <div class="page-header">
   <h1>Administration Home</h1>
 </div>
-<div class="row">
+<div class="row-fluid">
   <div class="span12 pagination-centered">
     <h3>Welcome to the Administration Panel</h3>
     <p>Use the administration menu to explore this area.</p>
